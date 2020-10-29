a00c8a7bcc45a6a7da85ea05987eeaace00601bb
Ashok Srinivasan <asrinivasan@burning-glass.com>
2020-10-29 19:51:35 +0530

    Latest changes

--- src/main/java/pageObject/loginPage.java
+++ src/main/java/pageObject/loginPage.java
@@ -1,11 +1,9 @@
 package pageObject;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
-
 import resources.base;
 
 public class loginPage extends base {
